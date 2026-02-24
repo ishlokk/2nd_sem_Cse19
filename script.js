a0c0781a387734e45424570fc8e7b692a2ff7442
ishlokk <ishlokkesarwani@gmail.com>
2026-02-24 06:46:48 +0000

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("into to variables in js");
+/*
+var name = "ishlok";
+var name = "kesarwani";
+name="ishlok kesarwani";
+console.log(name);*/
+/*
+let name ="ishlok";
+let name ="kesarwani;
+name="ishlok kesarwani";
+console.log(name);*/
+let age ="20";
+console.log(typeof age);
+
