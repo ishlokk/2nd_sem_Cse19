fc379f9c263f89bfe1a0b716bf01894a8549612c
ishlokk <ishlokkesarwani@gmail.com>
2026-06-18 23:44:37 +0530

    done

---
+++ exp7/Rcomponent.jsx
@@ -0,0 +1,27 @@
+Rcomponent.jsx 
+import React from 'react' 
+function Rcomponent() { 
+  return ( 
+    <div> 
+        <h1 className="container">Student Information</h1> 
+        <div className="card"> 
+            <h3>Rahul Sharma</h3> 
+            <p>Course: Computer Science</p> 
+            <p>Marks: 85</p> 
+        </div> 
+        <div className='card'> 
+            <h3>Anita Verma</h3> 
+            <p>Course: Information Technology</p> 
+            <p>Marks: 92</p> 
+        </div> 
+        <div className='card'> 
+            <h3>Rohan gupta</h3> 
+            <p>Course: Electronics</p> 
+            <p>Marks: 78</p> 
+        </div> 
+<p> Name-Yatharth Raj Nandan</p> 
+<p>Class-Cse-21</p> 
+<p>Roll No.-2503201001360</p> 
+        </div> 
+  )
+}
--- exp7/app.jsx
+++ exp7/app.jsx
@@ -1,1 +1,17 @@
-
+App.jsx 
+import { useState } from 'react' 
+import reactLogo from './assets/react.svg' 
+import viteLogo from './assets/vite.svg' 
+import heroImg from './assets/hero.png' 
+import './App.css' 
+import Rcomponent from './components/Rcomponent' 
+ 
+function App() { 
+  const [count, setCount] = useState(0) 
+ 
+  return ( 
+    <> 
+     <Rcomponent/> 
+    </> 
+  ) 
+} 
